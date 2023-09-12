16dbeeff1fd68b037b366e3fc15a6f424424c176
Ethan <144817453+fibxxth@users.noreply.github.com>
2023-09-12 22:20:24 +0700

    Update water3.py

--- task-1/water3.py
+++ task-1/water3.py
@@ -6,4 +6,14 @@ def is_goal(s):
 
 def successors(s):
     x, y, z = s
-    return []
+    if (y<5 and x>=5): yield ((x-(5-y),5,z),5)
+    if (y+z<=5 and z!=0): yield((x,y+z,0),z)
+    if (y+z>5 and z!=0): yield((x,5,x-(5-y)),5-y)
+    if (z<3 and x>=3): yield((x-(3-z),y,3),3)
+    if (y+z<=3 and y!=0): yield((x,0,y+z),y)
+    if (y+z>3 and y!=0): yield((x,y-(3-z),3),3-z)
+    if (y!=0): yield((x+y,0,z),y)
+    if (z!=0): yield((x+z,y,0),z)
+
+
+
